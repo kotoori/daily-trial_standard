b8732d283a1031f76e1177a268367331b48db9a8
kotoori <kotoori0815@gmail.com>
2023-07-03 01:04:46 +0900

    Day06：課題①ドロワーメニュー ・デバッグ用のconsole.log()削除

--- Day06/js/script.js
+++ Day06/js/script.js
@@ -1,9 +1,5 @@
 $(function(){
-
-    console.log("jQuery ready");
     $('.header-nav-item.drawer').click(function(){
-        console.log("click!");
-        console.log(this.className);
         $(this).find('.header-nav-item-drawer-item').slideToggle();
     })
 })
