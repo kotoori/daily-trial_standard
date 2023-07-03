409442f769b80415648c1fad79634c98956d19dc
kotoori <kotoori0815@gmail.com>
2023-07-03 10:39:32 +0900

    Day06：課題②画面スクロールで「戻るボタン」が出たり消えたりするようにした。ボタンを押したときの処理は未実装。

--- Day06/js/script.js
+++ Day06/js/script.js
@@ -1,5 +1,21 @@
 $(function(){
+    //ナビメニューのドロワー開閉(Click時)
     $('.header-nav-item.drawer').click(function(){
         $(this).find('.header-nav-item-drawer-item').slideToggle();
+    })
+
+    //Topへ戻るボタン
+    $(window).scroll(function(){
+        let scrollAmount = $(window).scrollTop();
+        console.log(scrollAmount);
+        if(scrollAmount >= 80){
+            console.log("Active");
+            $('.scroll_top').addClass('active');
+        }else if(scrollAmount < 80){
+            console.log("Passive");
+            $('.scroll_top').removeClass('active');
+        }else{
+            ;//何もしない
+        }
     })
 })
