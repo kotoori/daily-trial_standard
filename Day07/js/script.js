eec44a30a1a5d5e6ee1a45884647365fb81ee60d
kotoori <kotoori0815@gmail.com>
2023-07-04 01:16:53 +0900

    Day07：モーダルで画像の拡大表示するコード実装、まだ拡大表示中にBodyがスクロールしてしまう問題が残っている

--- Day07/js/script.js
+++ Day07/js/script.js
@@ -1,9 +1,15 @@
 $(function(){
+    /*================================
+    ドロワーメニュー
+    =================================*/
     //ナビメニューのドロワー開閉(Click時)
     $('.header-nav-item.drawer').click(function(){
         $(this).find('.header-nav-item-drawer-item').slideToggle();
     })
 
+    /*================================
+    Topへ戻るボタン
+    =================================*/
     //Topへ戻るボタンの表示/非表示を切り替える
     function scrollTopButtonShow(){
         let scrollAmount = $(window).scrollTop();
@@ -22,6 +28,7 @@ $(function(){
         }
     })
 
+    /* ページ内スクロールを緩やかにする */
     $('a[href^="#"]').click(function(){
         let href = $(this).attr("href");
         let target = $(href == "#" || href == "" ? 'html' : href);
@@ -32,6 +39,41 @@ $(function(){
             function(){
                 isAutoScrolling = false;
                 scrollTopButtonShow();
-            });
+            }
+        );
     })
-})
+
+    /*================================
+    モーダル
+    =================================*/
+
+    // 変数に要素を入れる
+    let open = $('.course-content-item img'),
+        close = $('.modal-close'),
+        container = $('.modal-container');
+        content = $('.modal-content');
+
+    //開くボタンをクリックしたらモーダルを表示する
+    open.on('click',function(){
+        content.append(this.outerHTML);
+        container.fadeIn().css('display','flex');;
+        return false;
+    });
+
+    //閉じるボタンをクリックしたらモーダルを閉じる
+    close.on('click',function(){
+        container.fadeOut(function(){
+            content.empty();
+        });
+    });
+
+    //モーダルの外側をクリックしたらモーダルを閉じる
+    $(document).on('click',function(e) {
+        if(!$(e.target).closest('.modal-body').length) {
+            container.fadeOut(function(){
+                content.empty();
+            });
+        }
+    });
+
+});
