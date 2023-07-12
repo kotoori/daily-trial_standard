4b393becc376a99d8865667eadc2988aa7b4c4da
kotoori <kotoori0815@gmail.com>
2023-07-12 23:57:44 +0900

    Day06-07：復習 ・TOPへ戻るボタン完了

--- Day06-review/js/script.js
+++ Day06-review/js/script.js
@@ -4,13 +4,41 @@ $(function(){
   /*==============================
   ドロワーメニュー開閉
   ==============================*/
-  let drawerMenu = $('.drawer__menu');
-  let drawerList = $('.drawer__list');
+  const drawerMenu = $('.drawer__menu');
+  const drawerList = $('.drawer__list');
 
   drawerMenu.on('click',function(){
     console.log("click");
     drawerList.slideToggle();
   })
 
-  
-}); /* $(function(){ */
+  /*==============================
+  Topに戻るボタン
+  ==============================*/
+  /* 画面スクロールでボタン表示/非表示 */
+  const topButton = $('.return-top');
+  const scrollAmount = 80;
+  const fadeTime = 300;
+
+  $(window).scroll(function(){
+    if($(window).scrollTop() >= scrollAmount){
+      topButton.fadeIn(fadeTime);
+    }else{
+      topButton.fadeOut(fadeTime);
+    }
+  })
+
+  /*==============================
+  ページ内スクロールを緩やかにする
+  ==============================*/
+  const scrollTime = 500;
+
+  $('a[href^="#"]').click(function(){
+    let href = $(this).attr("href");
+    let target = $(href == "#" || href == "" ? 'html' : href);
+    let targetY = target.offset().top;
+    // animateで移動します
+    $('html, body').animate({scrollTop : targetY}, scrollTime, 'swing');
+    return false;
+  })
+}); /* jQuery  $(function(){ */
