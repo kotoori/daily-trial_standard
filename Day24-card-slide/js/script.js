683849b422d2e0ff9def8c3645299d7abea60a0e
kotoori <kotoori0815@gmail.com>
2023-07-22 21:51:23 +0900

    Day24：余力課題ベース（Day24からコピー）　コミットし直し。。。

---
+++ Day24-card-slide/js/script.js
@@ -0,0 +1,44 @@
+/*========================
+Swiper
+=========================*/
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+  autoplay: {
+    delay: 3000,
+  },
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+
+/*========================
+nav active
+=========================*/
+const section = ['card','news','price','access','contact'];
+const navItem = '.header__nav .header__nav__item';
+
+jQuery(window).scroll(function() {
+	let scrollTop = jQuery(window).scrollTop(); // スクロール上部の位置
+  scrollTop += 100; /* 固定ヘッダの高さ100px分をスクロール位置に足す */
+
+  let cnt = 1;
+  for(let target of section){
+    jQuery(`${navItem}:nth-child(${cnt}) a`).removeClass('is-active');
+    let areaTop = jQuery(`#${target}`).offset().top; // 対象エリアの上部の位置
+    let areaBottom = areaTop + jQuery(`#${target}`).innerHeight(); // 対象エリアの下部の位置
+    if((scrollTop > areaTop) && (scrollTop <= areaBottom)){
+      jQuery(`${navItem}:nth-child(${cnt}) a`).addClass('is-active');
+    }
+    cnt++;
+  }
+});
