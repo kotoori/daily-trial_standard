c4e2ffb58c56f99cf8e4e18d2df862d599e17926
kotoori <kotoori0815@gmail.com>
2023-07-24 14:04:26 +0900

    Day25：新しいwowアニメーションに対応、指定の仕方の仕様が変わった

--- Day25-wow-catalog/js/script.js
+++ Day25-wow-catalog/js/script.js
@@ -48,89 +48,123 @@ jQuery(window).scroll(function() {
 wow
 =========================*/
 const animePattern = [
-'bounce' ,
-'flash' ,
-'pulse' ,
-'shake' ,
-'swing' ,
-'tada' ,
-'wobble' ,
-'bounceIn' ,
-'bounceInDown' ,
-'bounceInLeft' ,
-'bounceInRight' ,
-'bounceInUp' ,
-'bounceOut' ,
-'bounceOutDown' ,
-'bounceOutLeft' ,
-'bounceOutRight' ,
-'bounceOutUp' ,
-'fadeIn' ,
-'fadeInDown' ,
-'fadeInDownBig' ,
-'fadeInLeft' ,
-'fadeInLeftBig' ,
-'fadeInRight' ,
-'fadeInRightBig' ,
-'fadeInUp' ,
-'fadeInUpBig' ,
-'fadeOut' ,
-'fadeOutDown' ,
-'fadeOutDownBig' ,
-'fadeOutLeft' ,
-'fadeOutLeftBig' ,
-'fadeOutRight' ,
-'fadeOutRightBig' ,
-'fadeOutUp' ,
-'fadeOutUpBig' ,
-'flip' ,
-'flipInX' ,
-'flipInY' ,
-'flipOutX' ,
-'flipOutY' ,
-'lightSpeedIn' ,
-'lightSpeedOut' ,
-'rotateIn' ,
-'rotateInDownLeft' ,
-'rotateInDownRight' ,
-'rotateInUpLeft' ,
-'rotateInUpRight' ,
-'rotateOut' ,
-'rotateOutDownLeft' ,
-'rotateOutDownRight' ,
-'rotateOutUpLeft' ,
-'rotateOutUpRight' ,
-'slideInDown' ,
-'slideInLeft' ,
-'slideInRight' ,
-'slideOutLeft' ,
-'slideOutRight' ,
-'slideOutUp' ,
-'hinge' ,
-'rollIn' ,
-'rollOut',
+  'bounce',
+  'flash',
+  'pulse',
+  'rubberBand',
+  'shakeX',
+  'shakeY',
+  'headShake',
+  'swing',
+  'tada',
+  'wobble',
+  'jello',
+  'heartBeat',
+  'backInDown',
+  'backInLeft',
+  'backInRight',
+  'backInUp',
+  'backOutDown',
+  'backOutLeft',
+  'backOutRight',
+  'backOutUp',
+  'bounceIn',
+  'bounceInDown',
+  'bounceInLeft',
+  'bounceInRight',
+  'bounceInUp',
+  'bounceOut',
+  'bounceOutDown',
+  'bounceOutLeft',
+  'bounceOutRight',
+  'bounceOutUp',
+  'fadeIn',
+  'fadeInDown',
+  'fadeInDownBig',
+  'fadeInLeft',
+  'fadeInLeftBig',
+  'fadeInRight',
+  'fadeInRightBig',
+  'fadeInUp',
+  'fadeInUpBig',
+  'fadeInTopLeft',
+  'fadeInTopRight',
+  'fadeInBottomLeft',
+  'fadeInBottomRight',
+  'fadeOut',
+  'fadeOutDown',
+  'fadeOutDownBig',
+  'fadeOutLeft',
+  'fadeOutLeftBig',
+  'fadeOutRight',
+  'fadeOutRightBig',
+  'fadeOutUp',
+  'fadeOutUpBig',
+  'fadeOutTopLeft',
+  'fadeOutTopRight',
+  'fadeOutBottomRight',
+  'fadeOutBottomLeft',
+  'Flippers',
+  'flip',
+  'flipInX',
+  'flipInY',
+  'flipOutX',
+  'flipOutY',
+  'Lightspeed',
+  'lightSpeedInRight',
+  'lightSpeedInLeft',
+  'lightSpeedOutRight',
+  'lightSpeedOutLeft',
+  'rotateIn',
+  'rotateInDownLeft',
+  'rotateInDownRight',
+  'rotateInUpLeft',
+  'rotateInUpRight',
+  'rotateOut',
+  'rotateOutDownLeft',
+  'rotateOutDownRight',
+  'rotateOutUpLeft',
+  'rotateOutUpRight',
+  'Specials',
+  'hinge',
+  'jackInTheBox',
+  'rollIn',
+  'rollOut',
+  'zoomIn',
+  'zoomInDown',
+  'zoomInLeft',
+  'zoomInRight',
+  'zoomInUp',
+  'zoomOut',
+  'zoomOutDown',
+  'zoomOutLeft',
+  'zoomOutRight',
+  'zoomOutUp',
+  'slideInDown',
+  'slideInLeft',
+  'slideInRight',
+  'slideInUp',
+  'slideOutDown',
+  'slideOutLeft',
+  'slideOutRight',
+  'slideOutUp',
 ];
 
 const animePatternNum = animePattern.length;
-
+const afterLoad = true;
 $(function(){
   /* cardセクションのwowアニメーション種類を変更＆初期化 */
 //  const target = $('#card .card');
   const target = $('#card .card__content');
   const title = $('#card .section__title');
   let beforePattern = '';
-  const wowDuration = "data-wow-duration";
-  const wowDelay = "data-wow-delay";
-  const wowOffset = "data-wow-offset";
-  const wowIteration = "data-wow-iteration";
 
 /*** wow-catalog設定値 *********************************/
   /* 1.wowアニメーションのオプション設定 */
-  const wowOptions = {[wowDuration]: "1s",   /* アニメーションの時間 */
-                      [wowDelay]: "0s",      /* アニメーションがスタートするまでの時間 */
-                      [wowOffset]: 0,     /* アニメーションがスタートする距離 */
-                      [wowIteration]: 1,  /* 	アニメーションの繰り返し回数 */
-                    };
+  const wowDuration = "--animate-duration: 1s";
+  const wowDelay = "--animate-delay: 0s";
+  const wowOffset = "data-wow-offset";
+  const wowIteration = "--animate-repeat: 1";
 
   /* 2.次のアニメーションに遷移する時間
     (wowDuration * wowIteration + wowDelay + 1000msくらいを設定するとちょうどいいかも) */
@@ -138,20 +172,11 @@ $(function(){
 /******************************************************/
 
   function wowCatalog(n){
-    target.removeClass(`wow ${beforePattern}`);  /* 現在のwowクラスを削除 */
-    target.removeAttr('style');  /* 初期化時？につくstyle属性も削除 */
-    for(let wowOption in wowOptions){
-      if( wowOptions.hasOwnProperty(wowOption) ) {
-        target.removeAttr(wowOption);  /* オプションもいったん削除 */
-      }
-    }
+    target.removeClass(`animate__animated animate__${beforePattern}`);  /* 現在のwowクラスを削除 */
+    target.removeAttr('style');
     // console.log(`wow ${animePattern[n]}`);
-    target.addClass(`wow ${animePattern[n]}`); /* 次のwowクラスを付加 */
-    for(let wowOption in wowOptions){
-      if( wowOptions.hasOwnProperty(wowOption) ) {
-        target.attr({[wowOption]:wowOptions[wowOption]});  /* オプション追加 */
-      }
-    }
+    target.addClass(`animate__animated animate__${animePattern[n]}`); /* 次のwowクラスを付加 */
+    target.attr({'style':`${wowDelay};${wowDuration};${wowIteration}`});  /* オプション追加 */
 
     title[0].innerHTML = `${n + 1}: ${animePattern[n]}`; /* sectionタイトルにwowクラス名を表示 */
     beforePattern = animePattern[n];  /* 付加したwowクラスを保存(後で消すため) */
@@ -168,17 +193,18 @@ $(function(){
     if(cnt >= cntMax){
       cnt = 0;
     }
+    time = setTimeout(countUp, intervalTime);
   }
 
   /* 指定時間ごとにcountUp()を呼び出す */
-  time = setInterval(countUp, intervalTime);
+  time = setTimeout(countUp, 1000);
 
   /* cardセクションにスクロール */
   function toCard(){
     let cardTop = jQuery('#card').offset().top; /* cardエリアのトップ位置 */
     $('html,body').animate({ scrollTop: cardTop }, '1');
   }
-  setTimeout(toCard, 1500); /* ロード直後にスクロールしてもブラウザ機能で元の位置に戻ってしまうので、時間をおいてからスクロール */
+  setTimeout(toCard, 900); /* ロード直後にスクロールしてもブラウザ機能で元の位置に戻ってしまうので、時間をおいてからスクロール */
 
 });
 
