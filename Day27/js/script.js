93756ad7560bcfe7d9e0acb65eded5f1fcfc872f
kotoori <kotoori0815@gmail.com>
2023-07-28 17:19:58 +0900

    Day27：ベース（Day26をコピー）

---
+++ Day27/js/script.js
@@ -0,0 +1,117 @@
+/*========================
+Swiper
+=========================*/
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+  autoplay: {
+    delay: 3000,
+  },
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+
+/*========================
+nav active
+=========================*/
+const section = ['card','news','price','access','contact'];
+const navItem = '.header__nav .header__nav__item';
+
+jQuery(window).scroll(function() {
+	let scrollTop = jQuery(window).scrollTop(); // スクロール上部の位置
+  scrollTop += 100; /* 固定ヘッダの高さ100px分をスクロール位置に足す */
+
+  let cnt = 1;
+  for(let target of section){
+    jQuery(`${navItem}:nth-child(${cnt}) a`).removeClass('is-active');
+    let areaTop = jQuery(`#${target}`).offset().top; // 対象エリアの上部の位置
+    let areaBottom = areaTop + jQuery(`#${target}`).innerHeight(); // 対象エリアの下部の位置
+    if((scrollTop > areaTop) && (scrollTop <= areaBottom)){
+      jQuery(`${navItem}:nth-child(${cnt}) a`).addClass('is-active');
+    }
+    cnt++;
+  }
+});
+
+/*========================
+wow
+=========================*/
+new WOW().init();
+
+/*========================
+drawer
+=========================*/
+const hamburger = $('.hamburger');
+const drawerBg = $('.nav-bg');
+const drawerMenu = $('.header__nav');
+const drawerItem = $('.header__nav__item a');
+const openClass = 'is-open';
+
+$(function(){
+  hamburger.on('click',function(e){
+    e.preventDefault();
+    if(hamburger.hasClass(openClass)){
+      drawerClose();
+    }else{
+      drawerOpen();
+    }
+    return false;
+  })
+
+  drawerItem.on('click',function(e){
+    drawerClose();
+    return true;
+  })
+
+  /* ドロワーメニューをオープンする関数 */
+  function drawerOpen(){
+    bodyFix();
+    hamburger.addClass(openClass);
+    drawerBg.addClass(openClass);
+    drawerMenu.addClass(openClass);
+  }
+  /* ドロワーメニューをクローズする関数 */
+  function drawerClose(){
+    drawerMenu.removeClass(openClass);
+    drawerBg.removeClass(openClass);
+    hamburger.removeClass(openClass);
+    bodyFixReset();
+  }
+
+  //bodyを固定する関数
+  function bodyFix() {
+    const scrollPosition = $(window).scrollTop();
+    $('body').css({
+      'position': 'fixed',
+      'width': '100%',
+      'z-index': '1',
+      'top': -scrollPosition
+    });
+  }
+
+  //body固定を解除する関数
+  function bodyFixReset() {
+    const offsetPosition = $('body').offset().top;
+    const scrollPosition = $(window).scrollTop();
+    $('body').css({
+      'position': 'relative',
+      'width': 'auto',
+      'top': 'auto'
+    });
+    /* scroll位置を調整 */
+    $('html, body').scrollTop(scrollPosition - offsetPosition);
+  }
+
+});
+
