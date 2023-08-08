b6da036656349cd43e2989f379689715a36c8fe5
kotoori <kotoori0815@gmail.com>
2023-08-09 00:56:05 +0900

    Day31:総復習　Contactフォームの必須項目が全て入力されたらボタン有効化＆色反転する処理を追加

--- Day31(General_review)/code/js/script.js
+++ Day31(General_review)/code/js/script.js
@@ -5,6 +5,12 @@ new WOW().init();
 
 
 jQuery(function(){
+  /*============================
+  drawer.js
+  ============================*/
+  $('.drawer').drawer();
+
+
   /*===============================
   WOW.js option
   ・PC時だけ、横並びカード等のWOWアニメーション時間をdelayIntervalずつずらす
@@ -25,14 +31,8 @@ jQuery(function(){
         delayTime = index * delayInterval;
         $(this).attr('data-wow-delay',delayTime + "s");
       });
-
     }
   }
-
-  /*============================
-  drawer.js
-  ============================*/
-  $('.drawer').drawer();
 
   /*===============================
   スムーススクロール
@@ -51,6 +51,79 @@ jQuery(function(){
 
     // animateで移動します
     jQuery('html, body').animate({scrollTop : targetY}, 500, 'swing');
+
+    return false;
+  });
+
+  /*===============================
+  Contactの必須項目を全て入力したら、
+  ボタン色を反転&有効化する処理
+  ===============================*/
+  /* .js-form-check要素が変更されるたびに、全必須項目(.js-form-check)をチェックする */
+  const formInputText = '.contact__form__input[type="text"].js-form-check';
+  const formInputEmail = '.contact__form__input[type="email"].js-form-check';
+  const formInputRadioType = '.contact__form__input[type="radio"][name="type"]:checked.js-form-check';
+  const formInputCheckBox = '.contact__form__input[type="checkbox"].js-form-check';
+  const formTextarea = '.contact__form__textarea.js-form-check';
+
+  const formSubmitBtn = '.contact__form__btn';
+  const activeClass = 'is-active';
+
+  jQuery('.js-form-check').on('change', function(){
+    let isInput = true;
+
+    if(isInput){
+      /* input[type="text"]を全てチェック */
+      for(let target of jQuery(formInputText)){
+        if(target.value === ""){
+          isInput = false;
+        }
+      }
+    }
+
+    if(isInput){
+      /* input[type="email"]を全てチェック 未入力があればreturn */
+      for(let target of jQuery(formInputEmail)){
+        if(target.value === ""){
+          isInput = false;
+        }
+      }
+    }
+
+    if(isInput){
+      /* textareaを全てチェック 未入力があればreturn */
+      for(let target of jQuery(formTextarea)){
+        if(target.value === ""){
+          isInput = false;
+        }
+      }
+    }
+
+    if(isInput){
+      /* radioボタン[name="Type"]をチェック 未選択ならreturn */
+      if(jQuery(formInputRadioType).val() === undefined){
+        isInput = false;
+      }
+    }
+
+    if(isInput){
+      /* input[type="checkbox"]を全てチェック 未チェックがあればreturn */
+      for(let target of jQuery(formInputCheckBox)){
+        if(jQuery(target).prop("checked") === false){
+          isInput = false;
+        }
+      }
+    }
+
+    if(isInput){
+      /* 必須項目が全て入力された時の処理 */
+      jQuery(formSubmitBtn).prop("disabled",false);
+      jQuery(formSubmitBtn).addClass(activeClass);
+    }else{
+      /* 必須項目が全て入力されていない時の処理 */
+      jQuery(formSubmitBtn).prop("disabled",true);
+      jQuery(formSubmitBtn).removeClass(activeClass);
+    }
 
     return false;
   });
