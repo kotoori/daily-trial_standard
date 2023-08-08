5160a007b596b50033fa4756cdbecd0f9f35cc66
kotoori <kotoori0815@gmail.com>
2023-08-08 22:03:27 +0900

    Day31：総復習　スムーススクロール、Wowアニメーションを追加、レスポンシブ時に画像幅が100％担っていなかった箇所を修正

--- Day31(General_review)/code/js/script.js
+++ Day31(General_review)/code/js/script.js
@@ -1,7 +1,58 @@
-/*============================
-drawer.js
-============================*/
-$(document).ready(function() {
+/*===============================
+WOW.js
+===============================*/
+new WOW().init();
+
+
+jQuery(function(){
+  /*===============================
+  WOW.js option
+  ・PC時だけ、横並びカード等のWOWアニメーション時間をdelayIntervalずつずらす
+  ===============================*/
+  const delayInterval = 0.2; /* 0.2s */
+
+  if(window.matchMedia('(min-width:768px)').matches) {
+    /* PC（768px以上)のとき、横並びカードのWOWオプションを指定 */
+
+    setWowDelayForCard('.feature__contents','.feature__card');
+    setWowDelayForCard('.product__contents','.product__card');
+
+    /* 指定された横並びCardにWOW.jsアニメーションのdelayTimeを設定 */
+    /* delayTimeはdelayIntervalずつ、ずらして設定する */
+    function setWowDelayForCard(list,card){
+      let delayTime = 0;
+      $(list).children(card).each(function(index){
+        delayTime = index * delayInterval;
+        $(this).attr('data-wow-delay',delayTime + "s");
+      });
+
+    }
+  }
+
+  /*============================
+  drawer.js
+  ============================*/
   $('.drawer').drawer();
-  console.log('drawer load');
-});
+
+  /*===============================
+  スムーススクロール
+  ===============================*/
+  jQuery('a[href^="#"]').on('click', function(e){
+    e.preventDefault();
+
+    let targetY = 0;
+    let href = jQuery(this).attr("href");
+    if(href === "#"){
+      targetY = jQuery('html').offset().top;
+    }else{
+      let headerHight = jQuery('header').innerHeight();
+      targetY = jQuery(href).offset().top - headerHight;
+    }
+
+    // animateで移動します
+    jQuery('html, body').animate({scrollTop : targetY}, 500, 'swing');
+
+    return false;
+  });
+
+});// jQuery
