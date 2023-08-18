b14a13751b8035c6614fe3fe63a35157100483f9
kotoori <kotoori0815@gmail.com>
2023-08-18 17:36:49 +0900

    Day31：中級総復習の復習　JS　コンタクトフォームの入力チェック＆ボタン有効／無効切り替えに対応

--- Day31(General_review)-review/js/script.js
+++ Day31(General_review)-review/js/script.js
@@ -60,3 +60,85 @@ jQuery(function(){
     }
   }
 });
+
+/*===============================
+Contactフォーム入力チェック
+===============================*/
+/*===============================
+  Contactの必須項目を全て入力したら、
+  有効化&activeクラスを付与する処理
+  ===============================*/
+  const formInputText = '.contact__form__input[type="text"].js-form-check';
+  const formInputEmail = '.contact__form__input[type="email"].js-form-check';
+  const formInputRadioType = '.contact__form__input[type="radio"][name="type"]:checked.js-form-check';
+  const formInputCheckBox = '.contact__form__input[type="checkbox"].js-form-check';
+  const formTextarea = '.contact__form__textarea.js-form-check';
+
+  const formSubmitBtn = '.contact__form__submit';
+  const activeClass = 'is-active';
+
+  /* .js-form-check要素が変更されるたびに、全必須項目(.js-form-check)をチェックする */
+  jQuery('.js-form-check').on('change', function(){
+    let isInput = true;
+
+    console.log("cnange");
+    if(isInput){
+      /* input[type="text"]を全てチェック */
+      for(let target of jQuery(formInputText)){
+        if(target.value === ""){
+          console.log("false 1");
+          isInput = false;
+        }
+      }
+    }
+
+    if(isInput){
+      /* input[type="email"]を全てチェック 未入力があればreturn */
+      for(let target of jQuery(formInputEmail)){
+        if(target.value === ""){
+          console.log("false 2");
+          isInput = false;
+        }
+      }
+    }
+
+    if(isInput){
+      /* textareaを全てチェック 未入力があればreturn */
+      for(let target of jQuery(formTextarea)){
+        if(target.value === ""){
+          console.log("false 3");
+          isInput = false;
+        }
+      }
+    }
+
+    if(isInput){
+      /* radioボタン[name="type"]をチェック 未選択ならreturn */
+      if(jQuery(formInputRadioType).val() === undefined){
+        console.log("false 4");
+        isInput = false;
+      }
+    }
+
+    if(isInput){
+      /* input[type="checkbox"]を全てチェック 未チェックがあればreturn */
+      for(let target of jQuery(formInputCheckBox)){
+        if(jQuery(target).prop("checked") === false){
+          console.log("false 5");
+          isInput = false;
+        }
+      }
+    }
+
+    if(isInput){
+      /* 必須項目が全て入力された時の処理 */
+      jQuery(formSubmitBtn).prop("disabled",false);
+      jQuery(formSubmitBtn).addClass(activeClass);
+    }else{
+      /* 必須項目が全て入力されていない時の処理 */
+      jQuery(formSubmitBtn).prop("disabled",true);
+      jQuery(formSubmitBtn).removeClass(activeClass);
+    }
+
+    return false;
+  });
