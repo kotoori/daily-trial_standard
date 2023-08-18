4a52a3e366a4c794d718c2b5498c498948920850
kotoori <kotoori0815@gmail.com>
2023-08-19 00:32:57 +0900

    Day31：中級総復習の復習　JS　コンタクトフォームをGoogle Formに送信する処理を追加

--- Day31(General_review)-review/js/script.js
+++ Day31(General_review)-review/js/script.js
@@ -65,84 +65,115 @@ jQuery(function(){
 Contactフォーム入力チェック
 ===============================*/
 /*===============================
-  Contactの必須項目を全て入力したら、
-  有効化&activeクラスを付与する処理
-  ===============================*/
-  const formInputText = '.contact__form__input[type="text"].js-form-check';
-  const formInputEmail = '.contact__form__input[type="email"].js-form-check';
-  const formInputRadio = '.contact__form__input[type="radio"].js-form-check';
-  const formInputCheckBox = '.contact__form__input[type="checkbox"].js-form-check';
-  const formTextarea = '.contact__form__textarea.js-form-check';
+Contactの必須項目を全て入力したら、
+有効化&activeクラスを付与する処理
+===============================*/
+const formInputText = '.contact__form__input[type="text"].js-form-check';
+const formInputEmail = '.contact__form__input[type="email"].js-form-check';
+const formInputRadio = '.contact__form__input[type="radio"].js-form-check';
+const formInputCheckBox = '.contact__form__input[type="checkbox"].js-form-check';
+const formTextarea = '.contact__form__textarea.js-form-check';
 
-  const formSubmitBtn = '.contact__form__submit';
-  const activeClass = 'is-active';
+const formSubmitBtn = '.contact__form__submit';
+const activeClass = 'is-active';
 
-  /* .js-form-check要素が変更されるたびに、全必須項目(.js-form-check)をチェックする */
-  jQuery('.js-form-check').on('change', function(){
-    let isInput = true;
+/* .js-form-check要素が変更されるたびに、全必須項目(.js-form-check)をチェックする */
+jQuery('.js-form-check').on('change', function(){
+  let isInput = true;
 
-//    /* checkValidity()を使ってチェック */
-//    /* ※ 今回はあえて自作のチェック処理を使うため、checkValidity()は使わないでおく */
-//    if(jQuery('.contact__form')[0].checkValidity() !== true){
-//      isInput = false;
-//    }
+//  /* checkValidity()を使ってチェック */
+//  /* ※ 今回はあえて自作のチェック処理を使うため、checkValidity()は使わないでおく */
+//  if(jQuery('.contact__form')[0].checkValidity() !== true){
+//    isInput = false;
+//  }
 
-    if(isInput){
-      /* input[type="text"]を全てチェック */
-      for(let target of jQuery(formInputText)){
-        if(target.value === ""){
-          isInput = false;
-        }
+  if(isInput){
+    /* input[type="text"]を全てチェック */
+    for(let target of jQuery(formInputText)){
+      if(target.value === ""){
+        isInput = false;
       }
     }
+  }
 
-    if(isInput){
-      /* input[type="email"]を全てチェック */
-      for(let target of jQuery(formInputEmail)){
-        if(target.value === ""){
-          isInput = false;
-        }
+  if(isInput){
+    /* input[type="email"]を全てチェック */
+    for(let target of jQuery(formInputEmail)){
+      if(target.value === ""){
+        isInput = false;
       }
     }
+  }
 
-    if(isInput){
-      /* textareaを全てチェック */
-      for(let target of jQuery(formTextarea)){
-        if(target.value === ""){
-          isInput = false;
-        }
+  if(isInput){
+    /* textareaを全てチェック */
+    for(let target of jQuery(formTextarea)){
+      if(target.value === ""){
+        isInput = false;
       }
     }
+  }
 
-    if(isInput){
-      /* input[type="radio"]を全てチェック */
-      for(let target of jQuery(formInputRadio)){
-        let attrName = jQuery(target).attr('name');
-        let targetName = `${formInputRadio}[name="${attrName}"]:checked`;
-        if(jQuery(targetName).val() === undefined){
-          isInput = false;
-        }
+  if(isInput){
+    /* input[type="radio"]を全てチェック */
+    for(let target of jQuery(formInputRadio)){
+      let attrName = jQuery(target).attr('name');
+      let targetName = `${formInputRadio}[name="${attrName}"]:checked`;
+      if(jQuery(targetName).val() === undefined){
+        isInput = false;
       }
     }
+  }
 
-    if(isInput){
-      /* input[type="checkbox"]を全てチェック */
-      for(let target of jQuery(formInputCheckBox)){
-        if(jQuery(target).prop("checked") === false){
-          isInput = false;
-        }
+  if(isInput){
+    /* input[type="checkbox"]を全てチェック */
+    for(let target of jQuery(formInputCheckBox)){
+      if(jQuery(target).prop("checked") === false){
+        isInput = false;
       }
     }
+  }
 
-    if(isInput){
-      /* 必須項目が全て入力された時の処理 */
-      jQuery(formSubmitBtn).prop("disabled",false);
-      jQuery(formSubmitBtn).addClass(activeClass);
-    }else{
-      /* 必須項目が全て入力されていない時の処理 */
-      jQuery(formSubmitBtn).prop("disabled",true);
-      jQuery(formSubmitBtn).removeClass(activeClass);
-    }
+  if(isInput){
+    /* 必須項目が全て入力された時の処理 */
+    jQuery(formSubmitBtn).prop("disabled",false);
+    jQuery(formSubmitBtn).addClass(activeClass);
+  }else{
+    /* 必須項目が全て入力されていない時の処理 */
+    jQuery(formSubmitBtn).prop("disabled",true);
+    jQuery(formSubmitBtn).removeClass(activeClass);
+  }
 
-    return false;
+  return false;
+});
+
+
+/*===============================
+googleフォーム
+===============================*/
+jQuery(function () {
+
+  jQuery('#js-form').submit(function (event) {
+    const formData = jQuery(this).serialize();
+    const actionUrl = jQuery(this).attr("action");
+    console.log(actionUrl);
+    $.ajax({
+      url: actionUrl,
+      data: formData,
+      type: "POST",
+      dataType: "xml",
+      statusCode: {
+        0: function () {
+          //$(".end-message").slideDown();
+          //$(".submit-btn").fadeOut();
+          //window.location.href = "thanks.html";
+        },
+        200: function () {
+          //$(".false-message").slideDown();
+        }
+      }
+    });
+    event.preventDefault();
   });
+
+});
