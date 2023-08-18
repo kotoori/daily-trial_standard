52b2241636f29a686be247f42618e377152e83fa
kotoori <kotoori0815@gmail.com>
2023-08-18 16:15:07 +0900

    Day31：中級総復習の復習　JS　 ・スムーススクロール対応 ・drawerメニュー内のリンククリックで、drawerメニューを閉じる対応

--- Day31(General_review)-review/js/script.js
+++ Day31(General_review)-review/js/script.js
@@ -3,4 +3,33 @@ jQuery(function(){
   drawer.js
   ===============================*/
   $('.drawer').drawer();
+
+  /* drawerメニュー内のリンククリックで、drawerメニューを閉じる */
+  const drawerMenu = '.header__nav__link';
+  $(drawerMenu).on('click', function() {
+    $('.drawer').drawer('close');
+  });
+
+  /*===============================
+  スムーススクロール
+  ===============================*/
+  jQuery('a[href^="#"]').on('click', function(e){
+    e.preventDefault();
+
+    let targetY = 0;
+    let href = jQuery(this).attr("href");
+    if(href === "#"){
+      targetY = jQuery('html').offset().top;
+    }else{
+      let headerHight = jQuery('header').innerHeight();
+      targetY = jQuery(href).offset().top - headerHight;
+    }
+
+    // animateで移動します
+    jQuery('html, body').animate({scrollTop : targetY}, 500, 'swing');
+
+    return false;
+  });
 });
+
+
