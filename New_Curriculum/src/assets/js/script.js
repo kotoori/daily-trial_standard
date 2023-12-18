66fa638643c930d1ce3b64843f22d47437d51970
kotoori <kotoori0815@gmail.com>
2023-12-18 16:38:11 +0900

    New Curriculum：ドロワーメニューのリンクをタップで、ドロワーメニューが閉じるよう修正

--- New_Curriculum/src/assets/js/script.js
+++ New_Curriculum/src/assets/js/script.js
@@ -1,16 +1,22 @@
 /*===============================
-Hamburger Button
-===============================*/
-/*===============================
 Hamburger Menu
 ===============================*/
 jQuery(function(){
 	const hamburgerButton = '.header__hamburger';
 	const navMenu = '.header__nav';
 	const openClass = 'is-open';
+	const drawerItem = '.js-drawer-close'
 
+	//ハンバーガーボタンクリックでドロワーメニュー開閉
 	jQuery(hamburgerButton).on('click', function(){
 		jQuery(this).toggleClass(openClass);
 		jQuery(navMenu).toggleClass(openClass);
 	})
+
+	//メニュータップでドロワーメニューを閉じる
+	jQuery(drawerItem).on('click',function(){
+		jQuery(hamburgerButton).removeClass(openClass);
+		jQuery(navMenu).removeClass(openClass);
+	})
 })
+
