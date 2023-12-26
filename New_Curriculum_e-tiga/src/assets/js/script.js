6b8e02ee56e8602534f4bd360206779694d5274f
kotoori <kotoori0815@gmail.com>
2023-12-26 10:56:00 +0900

    e-tiga：更新

---
+++ New_Curriculum_e-tiga/src/assets/js/script.js
@@ -0,0 +1,21 @@
+/*===============================
+Hamburger Menu
+===============================*/
+jQuery(function(){
+	const hamburgerButton = '.header__hamburger';
+	const navMenu = '.header__nav';
+	const openClass = 'is-open';
+	const drawerItem = '.menu-item a'
+
+	//ハンバーガーボタンクリックでドロワーメニュー開閉
+	jQuery(hamburgerButton).on('click', function(){
+		jQuery(this).toggleClass(openClass);
+		jQuery(navMenu).toggleClass(openClass);
+	})
+
+	//メニュータップでドロワーメニューを閉じる
+	jQuery(drawerItem).on('click',function(){
+		jQuery(hamburgerButton).removeClass(openClass);
+		jQuery(navMenu).removeClass(openClass);
+	})
+})
