9c28d5cdfb49b5302b6f0a56f6636d67f32fd572
kotoori <kotoori0815@gmail.com>
2024-01-13 01:54:36 +0900

    New Curriculum：Wowアニメーション対応

--- New_Curriculum/src/assets/js/script.js
+++ New_Curriculum/src/assets/js/script.js
@@ -107,3 +107,33 @@ jQuery(function(){
 		}
 	});
 });
+
+/*===============================
+WOW.js
+===============================*/
+new WOW().init();
+
+jQuery(function(){
+  /*===============================
+  WOW.js option
+  ===============================*/
+  const delayInterval = 0.2; /* 0.2s */
+
+  if(window.matchMedia('(min-width:768px)').matches) {
+    /* PC（768px以上)のとき、横並びカードのWOWオプションを指定 */
+
+    setWowDelayForCard('.usage__step','.usage__step__item');
+    setWowDelayForCard('.product__contents','.product__card');
+
+    /* 指定された横並びCardにWOW.jsアニメーションのdelayTimeを設定 */
+    /* delayTimeはdelayIntervalずつ、ずらして設定する */
+    function setWowDelayForCard(list,card){
+      let delayTime = 0;
+      $(list).children(card).each(function(index){
+        delayTime = index * delayInterval;
+        $(this).attr('data-wow-delay',delayTime + "s");
+      });
+
+    }
+  }
+});
